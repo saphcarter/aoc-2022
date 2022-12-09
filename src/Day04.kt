a36e3b863ce613d07bd566edb3ba49f125e7fed8
saphcarter <sapphire@vitruvian.me>
2022-12-09 12:51:02 +0800

    day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,35 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        input.forEach {
+            val (firstFrom, firstTo, secondFrom, secondTo) = it.split('-', ',').map(String::toInt)
+            if((firstFrom <= secondFrom && firstTo >= secondTo) || (firstFrom >= secondFrom && firstTo <= secondTo)){
+                counter++
+            }
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.sumOf {
+            val (firstFrom, firstTo, secondFrom, secondTo) = it.split('-', ',').map(String::toInt)
+            val pair = firstFrom..firstTo to secondFrom..secondTo
+            if (pair.first.toSet().intersect(pair.second.toSet()).isNotEmpty()) 1L else 0L
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = listOf("2-4,6-8","2-3,4-5","5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8")
+    val test1 = part1(testInput)
+    check(test1 == 2)
+    println("Test 1 passed")
 
-    val input = readInput("Day01")
-    println(part1(input))
+    /*val test2 = part2(testInput)
+    check(test2 == 4)
+    println("Test 2 passed")*/
+
+    val input = readInput("Input04")
+    //println(part1(input))
     println(part2(input))
 }
